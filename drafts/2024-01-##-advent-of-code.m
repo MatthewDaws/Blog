869aa2ef29aabde8c395b6cec56ea86425180984
Matthew Daws <matt.daws@cantab.net>
2024-11-30 16:59:52 +0000

    Some merging

---
+++ drafts/2024-01-##-advent-of-code.m
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Advent of Code
+latex
+---
+
+I thought I might have a go at <a href="https://adventofcode.com/2023">Advent of Code</a> in (now last) December.  It's been some years since I did some serious coding, and even longer since I did anything close to "competitive coding".  However, various things have reminded me that I do greatly enjoy "problem solving", and that doing something intellectually engaging, but purely for fun, is something I don't have to avoid out of some sense that it's not "achieving something".
+
+I had some initial plan to also use this as an excuse to learn Javascript more properly.  However, work, home life, life in general, intervened and with it a complete lack of time.  So coding in Python it was.  Indeed, even trying to keep on top of doing each puzzle on the day it was set quickly fell by the wayside, with conferences and travel.  Then a family accident on holiday lead to even less time, so I'm now completing the puzzles in January (and February...)  Over the leaf are some thoughts about the (for me) trickier puzzles.  Code can be <a href="https://github.com/MatthewDaws/AdventCode23">found on GitHub</a>.
+
+<!--more-->
+
